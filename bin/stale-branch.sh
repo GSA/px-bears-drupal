f0ad658ce79ff7971801b0316f47898b9619456f
FatmaBakir <fatma.bakir@gsa.gov>
2023-09-08 10:52:58 -0400

    [381] - Testing the workflow - 60

--- bin/stale-branch.sh
+++ bin/stale-branch.sh
@@ -5,25 +5,21 @@ export STALE_THRESHOLD_DAYS=30
 
 export CURRENT_DATE=$(date +%Y-%m-%d)  # Define and set the current date in the desired format
 # Run the git for-each-ref command and save the output to a variable
-output=$(git for-each-ref --format='%(refname:short) %(committerdate:short)' refs/remotes/origin/ | sort | uniq)
-
+output=$(git branch -r |  grep -v "main\|release")
 # Initialize an empty array
 branch_list=()
 
 # Read lines one by one and append them to the array
 while IFS= read -r line; do
-  if [ "$line" != "origin/main" ] && [ "$line" != "origin/release" ]; then
     branch_list+=("$line")
   fi
 done <<< "$output"
 
+git fetch --all --prune 
 for branch in "${branch_list[@]}"; do
-  branch_name=$(echo $branch | cut -d ' ' -f 1)
-  last_commit_date=$(echo $branch | cut -d ' ' -f 2)
-
-  # Calculate the age of the branch in days
-  branch_age=$(( ( $(date -d "$CURRENT_DATE" +%s) - $(date -d "$last_commit_date" +%s) ) / 86400 ))
-
+  now=$(date +'%s')
+  commit_time=$(git log -1 --date=raw ${branch} | grep ^Date | awk '{print $2}')
+  commit_days=$(( (${now} - ${commit_time}) / 86400 ))
   echo "Branch: $branch_name, Last Commit Date: $last_commit_date, Age: $branch_age days"
 
   # Check if the branch is stale
