4880944b008dac4742430600b9d14bff89378e35
FatmaBakir <fatma.bakir@gsa.gov>
2023-09-07 23:59:36 -0400

    [381] - Testing the workflow - 36

--- bin/stale-branch.sh
+++ bin/stale-branch.sh
@@ -6,7 +6,7 @@ STALE_THRESHOLD_DAYS=60
 CURRENT_DATE=$(date +%Y-%m-%d)  # Define and set the current date in the desired format
 branch_list=$(git for-each-ref --format='%(refname:short) %(committerdate:short)' refs/heads/)
 
-for branch in "${branch_list}"; do
+for branch in $branch_list; do
   branch_name=$(echo $branch | cut -d ' ' -f 1)
   last_commit_date=$(echo $branch | cut -d ' ' -f 2)
 
